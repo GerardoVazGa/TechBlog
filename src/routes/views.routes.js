35a9ba4cd6fd6079fdec313e473d2aa6b9dbb63c
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-09-21 18:33:55 -0600

    feat(views): add about and contact routes

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express"
 import { getRecentPosts, getPost, getPostsByCategory, getAllPosts } from "../controllers/posts.controller.js"
+import { renderAboutPage, renderContactPage } from "../controllers/views.controller.js"
 
 const router = Router()
 
 router.get('/', getRecentPosts)
 router.get('/blog', getAllPosts)
 router.get('/blog/page/:page', getAllPosts)
-router.get('/about', (req, res) => {
-    res.render('about.ejs', {current: 'about'})
-})
+router.get('/about', renderAboutPage)
+router.get('/contact', renderContactPage)
 router.get('/blog/category/:category', getPostsByCategory)
 router.get('/blog/category/:category/page/:page', getPostsByCategory)
 router.get('/post/:slug',  getPost)
